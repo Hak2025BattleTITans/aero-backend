407b5248dc6f6a1607feb9ad9e6e9d9637a6066d
Artem <wieredsh@gmail.com>
2025-09-28 16:51:55 +0300

    feat(optimizer): add optimizer module skeleton

---
+++ src/optimizer/__init__.py
@@ -0,0 +1,1 @@
+﻿from .optimizer import Optimizer
---
+++ src/optimizer/optimizer.py
@@ -0,0 +1,23 @@
+﻿class Optimizer:
+    def __init__(self):
+        pass
+
+    def overbooking_optimization(self, data):
+        # Логика оптимизации овербукинга
+        return data
+
+    def ranking_optimization(self, data):
+        # Логика оптимизации ранжирования
+        return data
+
+    def form_report(self, data):
+        # Логика формирования отчета
+        return data
+
+    def save_results(self, data, filename):
+        # Логика сохранения результатов в файл
+        pass
+
+    def load_data(self, filename):
+        # Логика загрузки данных из файла
+        pass
